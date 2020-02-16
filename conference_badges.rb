e7f86365268c739b1dd5bcaa58b96da21eaca02c
Joseph Choi <jdc07@live.com>
2020-02-16 20:50:23 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,18 +3,18 @@ def badge_maker(name)
   return "Hello, my name is #{name}."
 end
 
-def batch_badge_creater(attendees)
+def batch_badge_creator(attendees)
   array = []
   attendees.each { |name| array << badge_maker(name) }
   array
 end
 
 def assign_rooms(attendees)
-  array = []
-  attendees.each_with_index do |name, index|
-  return ["Hello,#{name}! You'll be assigned to room #{index + 1}!"]
-  end
-  array
+   array = []
+   attendees.each_with_index do |name, index|
+      array << "Hello, #{name}! You'll be assigned to room #{index+1}!" 
+   end
+   array
 end
 
 def printer(attendees)
