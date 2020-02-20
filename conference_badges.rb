9ababe32e37c5530dd3c3541fc24bbe3b9b46d7c
Steven Prindle <flankbusiness@gmail.com>
2020-02-20 07:36:05 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,27 @@
-# Write your code here.
+require 'pry'
+def badge_maker(name)
+  "Hello, my name is #{name}."
+  #binding.pry
+end
+
+def batch_badge_creator(arr) #map
+  new_arr = []
+   arr.each do |name|
+    new_arr << "Hello, my name is #{name}."
+    #binding.pry
+  end
+  new_arr
+end
+
+def assign_rooms(attendees)
+  counter = 0
+  attendees.map do |attendee|
+    #binding.pry
+   "Hello, #{attendee}! You'll be assigned to room #{counter += 1}!"
+  end
+end
+
+def printer(arr_attendees)
+  batch_badge_creator(arr_attendees).each {|phrase| puts phrase}
+  assign_rooms(arr_attendees).each {|phrase| puts phrase}
+end
