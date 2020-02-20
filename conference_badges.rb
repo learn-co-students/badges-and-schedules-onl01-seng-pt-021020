bbb668e230f8bddd257712efd2d2f6dc28059087
excellent-revision-4527 <tjcroley@outlook.com>
2020-02-20 02:27:06 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,20 @@
-# Write your code here.
+#require "pry"
+def badge_maker(str)
+  return "Hello, my name is #{str}."
+end
+
+def batch_badge_creator(arr)
+  new_arr = []
+  arr.each do |name|
+    new_arr << badge_maker(name)
+   #binding.pry
+  end
+  new_arr
+end
+
+def assign_rooms(attendees)
+  
+end
+
+def printer(attendees)
+end
